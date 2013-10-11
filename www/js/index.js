aa790f118944bbc236825ecc520d28ba65ecb03e
fdoze <franciscodoze@gmail.com>
2013-10-11 12:04:23 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -59,39 +59,32 @@ var app = {
         divEl.appendChild(aElem);
     },
     downloadFile: function(){
-        alert('download file, yeah!');
         window.requestFileSystem(
                      LocalFileSystem.PERSISTENT, 0, 
                      function onFileSystemSuccess(fileSystem) {
                      fileSystem.root.getFile(
-                                 "dummy.html", {create: true, exclusive: false}, 
-                                 function gotFileEntry(fileEntry){
-                                 var sPath = fileEntry.fullPath.replace("dummy.html","");
-                                 var fileTransfer = new FileTransfer();
-                                 fileEntry.remove();
- 
-                                 fileTransfer.download(
-                                           "http://www.w3.org/2011/web-apps-ws/papers/Nitobi.pdf",
-                                           sPath + "theFile.pdf",
-                                           function(theFile) {
-                                           console.log("download complete: " + theFile.toURI());
-                                            alert("download complete: " + theFile.toURI());
-
-                                           app.showLink(theFile.toURI());
-                                           },
-                                           function(error) {
-                                           console.log("download error source " + error.source);
-                                           console.log("download error target " + error.target);
-                                           console.log("upload error code: " + error.code);
-                                           }
-                                           );
-                                 }, 
-                                 fail);
-                     }, 
-                     fail);
- 
+                                 "dummy.html", {create: true, exclusive: false},app.gotFileEntry,app.downloadErr,app.fail);
     },
-    
+    downloadErr: function(error) {
+            console.log("download error source " + error.source);
+            console.log("download error target " + error.target);
+            console.log("upload error code: " + error.code);
+        },
+    gotFileEntry: function(fileEntry){
+        var sPath = fileEntry.fullPath.replace("dummy.html","");
+        var fileTransfer = new FileTransfer();
+        fileEntry.remove();
+        
+        fileTransfer.download(
+            "http://www.w3.org/2011/web-apps-ws/papers/Nitobi.pdf",
+            sPath + "theFile.pdf",
+            function(theFile) {
+            console.log("download complete: " + theFile.toURI());
+            alert("download complete: " + theFile.toURI()
+        );
+        
+        app.showLink(theFile.toURI());
+    },
     fail: function(evt) {
         console.log(evt.target.error.code);
     }
